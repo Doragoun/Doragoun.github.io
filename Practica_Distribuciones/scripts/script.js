a7a46a350248686277d8f77d92d0b32cb44efdbb
Doragoun <miguelgamonh@hotmail.com>
2024-04-15 18:06:11 -0600

    P4

--- Practica_Distribuciones/scripts/script.js
+++ Practica_Distribuciones/scripts/script.js
@@ -66,17 +66,17 @@ function init() {
     );
 
   var nodeDataArray = [
-    { key: 0, name: "Xubuntu", source: "./image/Xubuntu.png", description: "2", parent: 9},
-    { key: 1, name: "Kubuntu", source: "./image/kubuntu.jpg", description: "1", parent:4},
-    { key: 2, name: "Suse", source: "./image/Suse.png", description: "0",parent:10},
-    { key: 3, name: "Arch Linux", source: "./image/Arch_Linux.png", description: "2", parent: 7},
-    { key: 4, name: "Ubuntu", source: "./image/Ubuntu.png", description: "1", parent: 5},
-    { key: 5, name: "DEBIAN", source: "./image/Debian.png", description:"2", parent: 7},
-    { key: 6, name: "RED HAT", source: "./image/Red_Hat.png", description:"3",parent: 7},
-    { key: 7, name: "Linux", source: "./image/Linux.png", description:"2", parent: 8},
+    { key: 0, name: "Xubuntu", source: "https://www.linuxadictos.com/wp-content/uploads/Xubuntu_logo.svg_-830x830.png", description: "2", parent: 9},
+    { key: 1, name: "Kubuntu", source: "https://upload.wikimedia.org/wikipedia/commons/1/1f/Kubuntu_logo.svg", description: "1", parent:4},
+    { key: 2, name: "Suse", source: "https://education.hpe.com/assets/images/SUSE_logo-green.png", description: "0",parent:10},
+    { key: 3, name: "Arch Linux", source: "https://www.muylinux.com/wp-content/uploads/2012/03/Arch-Linux.png", description: "2", parent: 7},
+    { key: 4, name: "Ubuntu", source: "https://imageio.forbes.com/blogs-images/jasonevangelho/files/2018/07/ubuntu-logo.jpg?format=jpg&height=900&width=1600&fit=bounds", description: "1", parent: 5},
+    { key: 5, name: "DEBIAN", source: "https://www.chip.de/ii/6/1/8/2/2/1/4/debian-logo-vertical-ae0babd339367d29.gif", description:"2", parent: 7},
+    { key: 6, name: "RED HAT", source: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Red_Hat_logo.svg/640px-Red_Hat_logo.svg.png", description:"3",parent: 7},
+    { key: 7, name: "Linux", source: "https://hostings.ar/wp-content/uploads/2023/11/Linux-Logo-1996-present.png", description:"2", parent: 8},
     { key: 8, name: "GNU", source: "https://upload.wikimedia.org/wikipedia/commons/8/83/The_GNU_logo.png", description: "Es el padre de Linux"},
-    { key: 9, name: "Linux Mint", source: "./image/Linux_Mint.png", description:"2", parent: 4},
-    { key: 10, name: "Slack Ware", source: "./image/slackware.jpg" , description:"2", parent: 7},
+    { key: 9, name: "Linux Mint", source: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Linux_Mint_logo_without_wordmark.svg/2048px-Linux_Mint_logo_without_wordmark.svg.png", description:"2", parent: 4},
+    { key: 10, name: "Slack Ware", source: "https://www.muylinux.com/wp-content/uploads/2011/04/slackware-13.37.jpg" , description:"2", parent: 7},
   ];
 
   myDiagram.model = new go.TreeModel(nodeDataArray);
